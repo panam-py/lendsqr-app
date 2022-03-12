60cb20d57ac0353359d20a7c9f431bd26e3642b2
Hebron Praise <panampraisehebron@gmail.com>
2022-03-12 20:02:25 +0100

    removed axios import

--- test/user.test.js
+++ test/user.test.js
@@ -3,7 +3,6 @@ process.env.ENV = "test";
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const server = require("../server");
-const axios = require("axios");
 
 let expect = chai.expect;
 
